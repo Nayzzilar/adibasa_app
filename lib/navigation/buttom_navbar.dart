44086656acf87d0d75c3286f000f5fc7e1d54080
LalaSalwa <lalasalwactk@email.com>
2025-04-30 06:01:06 +0700

    benerin icon bottomNav sama warnya pake theme

--- lib/navigation/buttom_navbar.dart
+++ lib/navigation/buttom_navbar.dart
@@ -9,72 +9,86 @@ class BottomNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => Container(
-        decoration: const BoxDecoration(
-          border: Border(top: BorderSide(color: Colors.grey, width: 1)),
+    return Obx(() {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              width: 2
+            ),
+          ),
         ),
         child: BottomNavigationBar(
-          backgroundColor: const Color(0xFFF2E2C4),
+          backgroundColor: Theme.of(context).colorScheme.surface,
           type: BottomNavigationBarType.fixed,
           elevation: 0,
           selectedFontSize: 12,
           unselectedFontSize: 0,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.grey,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           currentIndex: controller.selectedIndex.value,
           onTap: controller.changePage,
           items: [
             BottomNavigationBarItem(
-              icon: buildNavIcon(
-                0,
-                'assets/images/beranda.png',
-                'assets/images/beranda_selected.png',
-                'Beranda',
-              ),
+              icon: buildNavIcon(context, 0, Icons.home_outlined, 'Beranda'),
               label: '',
             ),
             BottomNavigationBarItem(
-              icon: buildNavIcon(
-                1,
-                'assets/images/kamus.png',
-                'assets/images/kamus_selected.png',
-                'Kamus',
-              ),
+              icon: buildNavIcon(context, 1, Icons.menu_book_outlined, 'Kamus'),
               label: '',
             ),
           ],
         ),
-      ),
-    );
+      );
+    });
   }
 
-  Widget buildNavIcon(
-    int index,
-    String icon,
-    String selectedIcon,
-    String label,
-  ) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          width: controller.selectedIndex.value == index ? 65 : 30,
-          height: controller.selectedIndex.value == index ? 30 : 30,
-          child: Image.asset(
-            controller.selectedIndex.value == index ? selectedIcon : icon,
-          ),
-        ),
-        const SizedBox(height: 2),
-        if (controller.selectedIndex.value == index)
-          Text(
-            label,
-            style: const TextStyle(fontSize: 12, color: Color(0xff462f00)),
-          ),
-      ],
-    );
-  }
+  Widget buildNavIcon(BuildContext context, int index, IconData icon, String label) {
+  final bool isSelected = controller.selectedIndex.value == index;
+
+  Widget iconWidget = Icon(
+    icon,
+    color: isSelected
+        ? Theme.of(context).colorScheme.onPrimary
+        : Theme.of(context).colorScheme.primary,
+    size: 24,
+  );
+
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      isSelected
+          ? Container(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 7),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.tertiaryContainer,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    offset: const Offset(0, 3),
+                    blurRadius: 0,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: iconWidget,
+            )
+          : iconWidget,
+      const SizedBox(height: 4),
+      isSelected
+          ? Text(
+              label,
+              style: const TextStyle(
+                color: Color(0xff462f00),
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            )
+          : const SizedBox.shrink(),
+    ],
+  );
+}
+
 }
