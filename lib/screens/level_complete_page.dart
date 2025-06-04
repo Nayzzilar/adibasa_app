7e7d8b54e322d774dfa879138f431549d041af47
Yofiyudistian <142886659+Yofiyudistian@users.noreply.github.com>
2025-06-04 08:46:14 +0700

    add confetti

--- lib/screens/level_complete_page.dart
+++ lib/screens/level_complete_page.dart
@@ -6,23 +6,39 @@ import '../widgets/gamification/card_level_complete.dart';
 import '../navigation/route_name.dart';
 import 'package:get/get.dart';
 import 'package:adibasa_app/theme/theme.dart';
+import 'package:confetti/confetti.dart';
+import 'dart:math';
 
-class LevelCompletePage extends ConsumerWidget {
+class LevelCompletePage extends ConsumerStatefulWidget {
   const LevelCompletePage({Key? key}) : super(key: key);
 
-  /*String formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$minutes:$seconds";
-  }*/
+  @override
+  ConsumerState<LevelCompletePage> createState() => _LevelCompletePageState();
+}
+
+class _LevelCompletePageState extends ConsumerState<LevelCompletePage> {
+  late ConfettiController _confettiController;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    _confettiController = ConfettiController(
+      duration: const Duration(seconds: 3),
+    );
+    _confettiController.play();
+  }
+
+  @override
+  void dispose() {
+    _confettiController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    // Mendapatkan state dari lessonGameProvider
     final gameState = ref.read(lessonGameProvider);
     final stars = gameState.stars;
     final duration = gameState.duration;
@@ -34,46 +50,40 @@ class LevelCompletePage extends ConsumerWidget {
           children: [
             Container(
               margin: EdgeInsets.only(top: screenHeight * 0.1),
-              height: 345.5,
-              width: 258.5,
-              child: Image.asset(
-                'assets/images/badak_tepuk_tangan.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-
-            /*Text(
-              'Level ${gameState.currentLesson?.order ?? ""} telah diselesaikan!',
-              style: TextStyle(
-                fontSize: screenWidth * 0.055,
-                fontWeight: FontWeight.bold,
-              ),
-            ),*/
-            /*Container(
-              margin: EdgeInsets.only(top: screenHeight * 0.02),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              height: 250.5,
+              width: 150.5,
+              child: Stack(
+                alignment: Alignment.center,
                 children: [
-                  Icon(Icons.access_time, size: screenWidth * 0.07),
-                  SizedBox(
-                    width: screenWidth * 0.015,
-                  ), // sedikit jarak antara ikon dan teks
-                  Text(
-                    formatDuration(duration),
-                    style: TextStyle(
-                      fontSize: screenWidth * 0.05,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  ConfettiWidget(
+                    confettiController: _confettiController,
+                    blastDirectionality: BlastDirectionality.explosive,
+                    emissionFrequency: 0.1,
+                    numberOfParticles: 20,
+                    gravity: 0.3,
+                    maxBlastForce: 10,
+                    minBlastForce: 5,
+
+                    colors: const [
+                      Color(0xFF59471D),
+                      Color(0xFF498C4C),
+                      Color(0xFF49758C),
+                      Color(0xFFE0900F),
+                    ],
+                  ),
+                  Image.asset(
+                    'assets/images/badak_tepuk_tangan.png',
+                    fit: BoxFit.contain,
                   ),
                 ],
               ),
-            ),*/
+            ),
 
             Container(
-              margin: EdgeInsets.only(top: screenHeight * 0.00001),
+              margin: EdgeInsets.only(top: screenHeight * 0.00005),
               child: CardLevelComplete(
-                duration: duration, // Contoh waktu
-                correctPercentage: correctPercentage, // Contoh skor
+                duration: duration,
+                correctPercentage: correctPercentage,
               ),
             ),
 
@@ -87,39 +97,13 @@ class LevelCompletePage extends ConsumerWidget {
               ),
             ),
 
-            /*Container(
-              margin: EdgeInsets.only(bottom: screenHeight * 0.05),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _buildIconButton(
-                    context,
-                    Icons.home_outlined,
-                    () => Get.offAllNamed(RouteName.bottomNavbar),
-                  ),
-                  SizedBox(width: screenWidth * 0.04),
-                  /*_buildIconButton(
-                    context,
-                    Icons.refresh,
-                    () => _replayLesson(ref),
-                  ),*/
-                  SizedBox(width: screenWidth * 0.04),
-                  /*_buildIconButton(
-                    context,
-                    Icons.double_arrow,
-                    () => _nextLesson(ref),
-                  ),*/
-                ],
-              ),
-            ),
-            */
             Container(
               margin: EdgeInsets.only(top: screenHeight * 0.05),
               decoration: BoxDecoration(
                 boxShadow: [
                   BoxShadow(
                     color: CustomColors.buttonColor.withOpacity(1.0),
-                    offset: Offset(0, 6), // arah dan jarak bayangan
+                    offset: const Offset(0, 4),
                   ),
                 ],
                 borderRadius: BorderRadius.circular(12),
@@ -136,13 +120,16 @@ class LevelCompletePage extends ConsumerWidget {
                       width: 2.0,
                     ),
                   ),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                  minimumSize: Size(330, 48),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 12,
+                  ),
+                  minimumSize: const Size(330, 48),
                   backgroundColor: CustomColors.borderButton,
                   foregroundColor: Colors.white,
-                  elevation: 0, // penting agar tidak dobel shadow
+                  elevation: 0,
                 ),
-                child: Text(
+                child: const Text(
                   'Lanjutkan',
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
@@ -153,37 +140,4 @@ class LevelCompletePage extends ConsumerWidget {
       ),
     );
   }
-
-  /*Widget _buildIconButton(
-    BuildContext context,
-    IconData icon,
-    VoidCallback onPressed,
-  ) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
-    return Container(
-      width: screenWidth * 0.15,
-      height: screenWidth * 0.06, // lebih pipih seperti di gambar
-      decoration: BoxDecoration(
-        color: const Color(0xFF9BA85B), // warna mirip olive green
-        borderRadius: BorderRadius.circular(30), // membuatnya oval/kapsul
-        boxShadow: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.tertiary,
-            offset: const Offset(0, 3),
-            blurRadius: 0,
-            spreadRadius: 0,
-          ),
-        ],
-      ),
-      child: Center(
-        child: IconButton(
-          icon: Icon(icon, color: Colors.white), // ikon putih
-          iconSize: screenWidth * 0.045,
-          padding: EdgeInsets.zero, // menghilangkan padding default
-          onPressed: onPressed,
-        ),
-      ),
-    );
-  }*/
 }
